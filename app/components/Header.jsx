97b442888aa5b0c4da56870a3497d268e3b085d3
Allan Heremi <allanheremidev@gmail.com>
2023-09-28 18:17:30 +0200

    Fix: FA icons + header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,6 +1,9 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
+
 
 function Header() {
   const [ethPrice, setEthPrice] = useState(null);
@@ -26,29 +29,37 @@ function Header() {
 
   return (
     <header className="h-24">
-      <div className="flex justify-between h-[50%] bg-red-200 p-4 lg:px-8">
+      <div className="flex justify-between h-[50%] p-4 lg:px-8">
         <div className="w-[50%]">
           {ethPrice !== null ? (
             <div className="flex flex-row gap-4">
               <p>ETH/USD: {ethPrice}</p>{' '}
               <p className="flex">
-                {priceChange >= 0 ? <p>+</p> : <p>-</p>}
-                {priceChange.toFixed(2)}%{' '}
+                {priceChange >= 0 ? (
+                  <p className="text-[#006400]">+</p>
+                ) : (
+                  <p className="text-[#FF0000]">-</p>
+                )}
+                {priceChange >= 0 ? (
+                  <p className="text-[#006400]">{priceChange.toFixed(2)}%</p>
+                ) : (
+                  <p className="text-[#FF0000]">{priceChange.toFixed(2)}%</p>
+                )}{' '}
               </p>{' '}
             </div>
           ) : (
             <span>Loading...</span>
           )}
         </div>
-        <div>light mode</div>
+        <div><FontAwesomeIcon icon={faMoon}/></div>
       </div>
 
-      <div className="flex justify-between h-[50%] bg-blue-200 p-4 lg:px-8">
+      <div className="flex justify-between h-[50%] p-4 lg:px-8 border-t border-grey border-w">
         <div className="flex items-center justify-start gap-1">
           <img
             src={'/Lightblock.png'}
             alt="logo"
-            className="flex w-[3rem] rounded-sm"
+            className="flex w-[2.5rem] rounded-sm"
           />
           <h1 className="h-[full] lg:text-[2rem] text-[1rem]">Lightblock</h1>
         </div>
