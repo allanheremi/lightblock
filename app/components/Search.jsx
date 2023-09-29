c247db1a0e7a585d9909b669fccb8df1ff2c8433
Allan Heremi <allanheremidev@gmail.com>
2023-09-29 10:43:18 +0200

    Fix: search input state

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import 'tailwindcss/tailwind.css';
 
 function Search() {
+  const [search, setSearch] = useState('')
   
   return (
     <div className="bg-search-bg bg-cover bg-center h-[12rem]">
-      <div className="text-white text-sm lg:text-lg p-8">
+      <div className="text-sm lg:text-lg p-8">
         <input type="text"
         placeholder="Search by block number / address / Tx hash"
         className='w-full lg:w-2/3 border-gray-200 rounded-sm p-2 border-b-2'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
          />
       </div>
     </div>
