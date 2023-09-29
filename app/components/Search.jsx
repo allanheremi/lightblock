90e8592a038b93197a822d60240392b55b2f2d06
Allan Heremi <allanheremidev@gmail.com>
2023-09-29 10:23:51 +0200

    Feat: search input added

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import 'tailwindcss/tailwind.css';
 
 function Search() {
+  
   return (
-    <div className='relative'>
-      <img
-        src={'/searchBG.png'}
-        alt="background"
-        className="bg-cover h-[12rem] w-full absolute"
-      />    
+    <div className="bg-search-bg bg-cover bg-center h-[12rem]">
+      <div className="text-white text-sm lg:text-lg p-8">
+        <input type="text"
+        placeholder="Search by block number / address / Tx hash"
+        className='w-full lg:w-2/3 border-gray-200 rounded-sm p-2 border-b-2'
+         />
+      </div>
     </div>
   );
 }
