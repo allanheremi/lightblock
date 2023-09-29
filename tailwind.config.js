3b3eb851b555054a3b728a04272338b041e63f2a
Allan Heremi <allanheremidev@gmail.com>
2023-09-29 10:23:13 +0200

    Fix: added bg for search input

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,9 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+        'search-bg': "url('/searchBG.png')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
     },
   },
   plugins: [],
