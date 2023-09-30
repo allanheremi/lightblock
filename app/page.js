2e806d4e9f0559d11396e464d0628651d5f89638
Allan Heremi <allanheremidev@gmail.com>
2023-09-30 21:25:20 +0200

    Fix: disabled ssr for transactionData component

--- app/page.js
+++ app/page.js
@@ -4,13 +4,19 @@ import Data from './components/TransactionData';
 import Blocks from './components/Blocks';
 import Transactions from './components/Transactions';
 import Footer from './components/Footer';
+import dynamic from 'next/dynamic';
+
+const TransactionData = dynamic(() => import('./components/TransactionData'), {
+  ssr: false
+});
+
 
 export default function Home() {
   return (
     <>
       <Header />
       <Search />
-      <Data />
+      <TransactionData />
       {/* <Blocks />
       <Transactions />
       <Footer /> */}
