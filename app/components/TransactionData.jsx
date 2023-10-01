1b390911a14e5c02f982b36d35f0a22992c82779
Allan Heremi <allanheremidev@gmail.com>
2023-10-01 12:49:47 +0200

    Feat: transactionData component done pending refactoring

--- app/components/TransactionData.jsx
+++ app/components/TransactionData.jsx
@@ -12,6 +12,12 @@ const settings = {
 const alchemy = new Alchemy(settings);
 
 function TransactionData() {
+  const [gasPrice, setGasPrice] = useState({
+    gasPrice: null,
+    maxFeePerGas: null,
+    maxPriorityFeePerGas: null,
+  });
+
   const [blocks, setBlocks] = useState({
     block1: null,
     block2: null,
@@ -20,13 +26,21 @@ function TransactionData() {
     block5: null,
   });
 
-  const [transactions, setTransactions] = useState({
-    transaction1: null,
-    transaction2: null,
-    transaction3: null,
-    transaction4: null,
-    transaction5: null,
-  });
+  const fetchChainData = async () => {
+    try {
+      const data = await alchemy.core.getFeeData();
+
+      setGasPrice({
+        gasPrice: data.gasPrice / 1000000000,
+        maxFeePerGas: parseInt(data.maxFeePerGas, 16).toString().slice(0, 2),
+        maxPriorityFeePerGas: parseInt(data.maxPriorityFeePerGas, 16)
+          .toString()
+          .slice(0, 2),
+      });
+    } catch (error) {
+      console.error('Could not retrieve chain data ', error);
+    }
+  };
 
   const fetchBlocks = async () => {
     try {
@@ -49,31 +63,37 @@ function TransactionData() {
   };
 
   useEffect(() => {
-    fetchBlocks();
+    const fetchData = async () => {
+      await fetchBlocks();
+      await fetchChainData();
+    };
+    fetchData();
+    const intervalId = setInterval(fetchData, 4000);
+    return () => {
+      clearInterval(intervalId);
+    };
   }, []);
-
-  fetchBlocks();
 
   return (
     <>
       <div className="w-full flex flex-row p-0 gap-0">
         <div className="w-1/2 border-b-2 border-l-2 p-2">
           <div className="p-0 lg:p-8">
-            <table className="flex flex-row justify-between">
+            <table className="flex flex-row justify-around text-center">
               <tr>
-                <th className="flex justify-center">Latest blocks:</th>
+                <th className="flex justify-center">Last block:</th>
                 {Object.keys(blocks).map((blockKey, index) => (
-                  <td className="flex p-2 underline" key={index}>
+                  <td className="flex p-1 underline" key={index}>
                     {blocks[blockKey]}
                   </td>
                 ))}
               </tr>
 
               <th className="text-left">
-                Etherscan:{' '}
+                Details:{' '}
                 {Object.keys(blocks).map((blockKey, index) => (
                   <td
-                    className="flex p-2 underline justify-center font-medium"
+                    className="flex p-1 underline justify-center font-medium"
                     key={index}
                   >
                     <button className="outline-slate-600 outline rounded-sm px-0 lg:px-2 hover:translate-y-[-0.1rem] hover:bg-slate-100 duration-300">
@@ -92,14 +112,31 @@ function TransactionData() {
         </div>
 
         <div className="w-1/2 border-b-2 border-l-2 p-2">
-          <h2 className="text-left">Latest transactions</h2>
-          <div className="p-8">
-            <table className="flex flex-row justify-between">
-              <thead>
-                <tr>
-                  <th>Transaction hash:</th>
-                </tr>
-              </thead>
+          <div className="p-0 lg:p-8">
+            <table className="flex flex-row justify-around text-center">
+              <tr>
+                <th className="flex justify-center underline pb-3">
+                  Chain fees:
+                </th>
+                <td className="flex py-3 px-2 justify-start font-bold w-full text-left">
+                  {' '}
+                  Base ⛽{' '}
+                  {gasPrice.gasPrice
+                    ? Number(gasPrice.gasPrice).toFixed() + 'GWEI '
+                    : null}{' '}
+                </td>
+
+                <td className="flex py-3 px-2 fy-start font-bold  w-full">
+                  {' '}
+                  Max ⛽ {gasPrice.maxFeePerGas ? gasPrice.maxFeePerGas : null}
+                </td>
+                <td className="flex py-3 px-2 fy-start font-bold">
+                  Priority ⛽{' '}
+                  {gasPrice.maxPriorityFeePerGas
+                    ? gasPrice.maxPriorityFeePerGas
+                    : null}
+                </td>
+              </tr>
             </table>
           </div>
         </div>
